c64239f9a6031399b3d3f7ec118984a1cea0f111
Karina Kiselyova <kimitoshiko@gmail.com>
2024-07-28 02:02:43 +0500

    feat: task01 with test

---
+++ task01/jsrc/main/java/com/task01/CmtrF7814Be5HelloWorldTest.java
@@ -0,0 +1,25 @@
+package com.task01;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "cmtr-f7814be5-hello_world-test",
+	roleName = "cmtr-f7814be5-hello_world-test-role",
+	isPublishVersion = false,
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class CmtrF7814Be5HelloWorldTest implements RequestHandler<Object, Map<String, Object>> {
+
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("message", "Hello from Lambda");
+		return resultMap;
+	}
+}
