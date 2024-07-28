48cb647eb3386fad16a373d27b7bbba6da0fd14b
Karina Kiselyova <kimitoshiko@gmail.com>
2024-07-28 18:49:43 +0500

    fixe: task02 body to message

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -5,12 +5,12 @@ exports.handler = async (event) => {
 
     const goodRes = {
         statusCode: 200,
-        body: JSON.stringify("Hello from Lambda")
+        message: JSON.stringify("Hello from Lambda")
     }
 
     const badRes = {
         statusCode: 400,
-        body: JSON.stringify(`Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`)
+        message: JSON.stringify(`Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`)
     }
 
     if (path === JSON.stringify("/hello")) {
